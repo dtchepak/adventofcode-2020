e1551ec4971b1c68e46450926043af1ad4bbb62c
David Tchepak <dave@davesquared.net>
2020-12-08 17:30:46 +1100

    Day 5
    Simplified. Didn't end up having to partition arbitrary ranges. Oops.

--- src/Aoc/Day05.hs
+++ src/Aoc/Day05.hs
@@ -1,127 +1,40 @@
 module Aoc.Day05
-  ( binarySpace,
-    right,
-    left,
-    upper,
-    lower,
-    seatId,
-    Tree (..),
-    Partition (..),
-    PathElement (..),
-    mkPartition,
-    followPath,
-    parseBoardingPasses,
+  ( seatId,
+    parseRowAndCol,
     part1,
     part2,
   )
 where
 
-import Data.List (find, maximumBy, sortOn)
-import Data.Maybe (catMaybes)
-import Data.Monoid
-import Data.Ord (comparing)
+import Data.Bifunctor (Bifunctor (bimap))
+import Data.Bits
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.List (sort)
+import Data.Foldable (find)
 
-data Partition
-  = Range !Int !Int
-  | Value !Int
-  deriving (Show, Eq)
-
-mkPartition :: Int -> Int -> Partition
-mkPartition lo hi
-  | lo > hi || lo < 0 = Value 0
-  | lo == hi = Value lo
-  | otherwise = Range lo hi
-
-splitPartition :: Partition -> (Partition, Partition)
-splitPartition (Range lo hi) =
-  let half = (hi - lo) `div` 2
-   in (mkPartition lo (lo + half), mkPartition (lo + half + 1) hi)
-splitPartition v@(Value _) = (v, v)
-
-upper :: Partition -> Partition
-upper = snd . splitPartition
-
-lower :: Partition -> Partition
-lower = fst . splitPartition
-
-data Tree a
-  = Node (Tree a) (Tree a)
-  | Leaf a
-  deriving (Show, Eq)
-
-left :: Tree a -> Tree a
-left (Node x _) = x
-left x@(Leaf _) = x
-
-right :: Tree a -> Tree a
-right (Node _ x) = x
-right x@(Leaf _) = x
-
-forwardCompose :: [a -> a] -> a -> a
-forwardCompose = appEndo . getDual . foldMap (Dual . Endo)
-
-data PathElement = LeftPath | RightPath deriving (Show, Eq)
-
-followPath :: [PathElement] -> Tree a -> Tree a
-followPath =
-  let action LeftPath = left
-      action RightPath = right
-   in forwardCompose . fmap action
-
-binarySpace :: Int -> Int -> Tree Partition
-binarySpace lo hi =
-  let mkTree v@(Value _) = Leaf v
-      mkTree r@(Range _ _) =
-        let (lower', upper') = splitPartition r
-         in Node (mkTree lower') (mkTree upper')
-   in mkTree (mkPartition lo hi)
+bitsToInt :: (Char -> Int) -> Text -> Int
+bitsToInt toBit =
+  T.foldl' (\acc -> (.|.) (shift acc 1) . toBit) zeroBits
 
 seatId :: Int -> Int -> Int
 seatId r c = r * 8 + c
 
-parsePath :: Text -> [PathElement]
-parsePath =
-  let mapChar c
-        | c == 'F' = LeftPath
-        | c == 'L' = LeftPath
-        | otherwise = RightPath -- 'B', 'R'
-   in T.foldr ((:) . mapChar) []
+parseRowAndCol :: Text -> (Int, Int)
+parseRowAndCol =
+  let isOn c = if c == 'B' || c == 'R' then 1 else 0 -- Back and Right correspond to bit on/1.
+      map2 f = bimap f f
+  in map2 (bitsToInt isOn) . T.splitAt 7
 
-data BoardingPass = BoardingPass
-  { row :: !Int,
-    col :: !Int,
-    seat :: !Int
-  }
-  deriving (Show, Eq)
+passes :: Text -> [Int]
+passes = fmap (uncurry seatId . parseRowAndCol) . T.lines
 
-mkBoardingPass :: Int -> Int -> BoardingPass
-mkBoardingPass r c = BoardingPass r c (seatId r c)
+part1 :: Text -> Int
+part1 = maximum . passes
 
-rows :: Tree Partition
-rows = binarySpace 0 127
-
-cols :: Tree Partition
-cols = binarySpace 0 7
-
-parseBoardingPass :: Text -> Maybe BoardingPass
-parseBoardingPass s =
-  let (rowText, colText) = T.splitAt 7 s
-      rowPath = followPath . parsePath $ rowText
-      colPath = followPath . parsePath $ colText
-      getValue (Leaf (Value v)) = pure v
-      getValue _ = Nothing
-   in mkBoardingPass <$> getValue (rowPath rows) <*> getValue (colPath cols)
-
-parseBoardingPasses :: Text -> [Maybe BoardingPass]
-parseBoardingPasses = fmap parseBoardingPass . T.lines
-
-part1 :: Text -> BoardingPass
-part1 = maximumBy (comparing seat) . catMaybes . parseBoardingPasses
-
-part2 :: Text -> Maybe (BoardingPass, BoardingPass)
+part2 :: Text -> Maybe Int
 part2 =
-  let sorted = sortOn seat . catMaybes . parseBoardingPasses
+  let sorted = sort . passes
       diffs x = zip x (drop 1 x)
-   in find (\(x, y) -> seat y - seat x > 1) . diffs . sorted
+      gap = find (\(x, y) -> y - x > 1) . diffs . sorted
+  in fmap (succ . fst) . gap
--- test/Aoc/Day05Spec.hs
+++ test/Aoc/Day05Spec.hs
@@ -8,29 +8,14 @@ spec = do
   describe "seat id" $ do
     it "example" $ do
       seatId 44 5 `shouldBe` 357
+      seatId 70 7 `shouldBe` 567
+      seatId 14 7 `shouldBe` 119
+      seatId 102 4 `shouldBe` 820
 
-  describe "row search" $ do
-    let rowTree = binarySpace 0 127
-
-    it "upper and lower partition examples" $ do
-      lower (Range 0 127) `shouldBe` Range 0 63
-      upper (Range 0 63) `shouldBe` Range 32 63
-      lower (Range 32 63) `shouldBe` Range 32 47
-      upper (Range 32 47) `shouldBe` Range 40 47
-      upper (Range 40 47) `shouldBe` Range 44 47
-      lower (Range 44 47) `shouldBe` Range 44 45
-      lower (Range 44 45) `shouldBe` Value 44
-
-    it "search space FBFBBFF" $ do
-      let path = [LeftPath, RightPath, LeftPath, RightPath, RightPath, LeftPath, LeftPath]
-      followPath path rowTree `shouldBe` Leaf (Value 44)
-
-    it "search space BFFFBBF" $ do
-      let path = [RightPath, LeftPath, LeftPath, LeftPath, RightPath, RightPath, LeftPath]
-      followPath path rowTree `shouldBe` Leaf (Value 70)
-
-  describe "column search" $ do
-    let cols = binarySpace 0 7
-    it "example RLR" $ do
-      let path = [RightPath, LeftPath, RightPath]
-      followPath path cols `shouldBe` Leaf (Value 5)
+  describe "examples" $ do
+    it "BFFFBBFRRR: row 70, column 7, seat ID 567" $ do
+      parseRowAndCol "BFFFBBFRRR" `shouldBe` (70, 7)
+    it "FFFBBBFRRR: row 14, column 7, seat ID 119" $ do
+      parseRowAndCol "FFFBBBFRRR" `shouldBe` (14, 7)
+    it "BBFFBBFRLL: row 102, column 4, seat ID 820" $ do
+      parseRowAndCol "BBFFBBFRLL" `shouldBe` (102, 4)
