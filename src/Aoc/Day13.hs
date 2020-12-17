3212553662c8ec90fd621b54a15b5808f3bb87e7
David Tchepak <dave@davesquared.net>
2020-12-18 10:13:56 +1100

    Naïve attempt at day 13 part 2 (too slow for the proper input)

--- src/Aoc/Day13.hs
+++ src/Aoc/Day13.hs
@@ -1,13 +1,16 @@
 module Aoc.Day13
   ( earliest,
     part1,
+    part2,
+    consecutive,
   )
 where
 
-import Data.Attoparsec.Text
-import Data.List (sortOn)
+import Data.Function (on)
+import Data.List (find, maximumBy, sortOn)
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.Read as T
 
 -- | Return the next timestamp after `time` that a particular bus will arrive.
 nextFromTime :: Int -> Int -> Int
@@ -30,9 +33,40 @@ part1Timetable = [23, 41, 733, 13, 17, 19, 29, 449, 37]
 
 part1 :: Maybe Int
 part1 =
-  let
-    ts = part1Timestamp
-    timetable = part1Timetable
-    bus = earliest ts timetable
-    minutes = (\t -> t - ts) . nextFromTime ts <$> bus
-  in (*) <$> bus <*> minutes
+  let ts = part1Timestamp
+      timetable = part1Timetable
+      bus = earliest ts timetable
+      minutes = (\t -> t - ts) . nextFromTime ts <$> bus
+   in (*) <$> bus <*> minutes
+
+part2Times :: Text
+part2Times = "23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,733,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,19,x,x,x,x,x,x,x,x,x,29,x,449,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37"
+
+type Index = Int
+
+parse :: Text -> [(Index, Int)]
+parse =
+  let parseInt = either (const 0) fst . T.decimal
+   in filter ((/= 0) . snd) . fmap (parseInt <$>) . zip [0 ..] . T.split (== ',')
+
+-- | Check bus arrives at ts+i.
+tryTimestamp :: Int -> (Index, Int) -> Bool
+tryTimestamp ts (i, bus) =
+  -- If it divides evenly without remainder then it will arrive at this time.
+  (ts + i) `mod` bus == 0
+
+tryTimestampForAll :: Int -> [(Index, Int)] -> Bool
+tryTimestampForAll ts = all (tryTimestamp ts)
+
+consecutive :: Text -> Maybe Int
+consecutive txt =
+  let input = parse txt
+      (indexOfLargest, largestTerm) = maximumBy (compare `on` snd) input
+      timestampFor multiplier = largestTerm * multiplier - indexOfLargest
+      findMultiplier = find (flip tryTimestampForAll input . timestampFor) [1 ..]
+   in timestampFor <$> findMultiplier
+
+--  in findMultiplier
+
+part2 :: Maybe Int
+part2 = consecutive part2Times
--- test/Aoc/Day13Spec.hs
+++ test/Aoc/Day13Spec.hs
@@ -1,4 +1,3 @@
-
 module Aoc.Day13Spec (spec) where
 
 import Aoc.Day13
@@ -9,3 +8,21 @@ spec = do
   describe "part 1" $ do
     specify "example" $ do
       earliest 939 [7, 13, 59, 31, 19] `shouldBe` Just 59
+  describe "part 2" $ do
+    specify "main example" $ do
+      consecutive "7,13,x,x,59,x,31,19" `shouldBe` Just 1068781
+
+    specify "The earliest timestamp that matches the list 17,x,13,19 is 3417" $ do
+      consecutive "17,x,13,19" `shouldBe` Just 3417
+
+    specify "67,7,59,61 first occurs at timestamp 754018." $ do
+      consecutive "67,7,59,61" `shouldBe` Just 754018
+
+    specify "67,x,7,59,61 first occurs at timestamp 779210." $ do
+      consecutive "67,x,7,59,61" `shouldBe` Just 779210
+
+    specify "67,7,x,59,61 first occurs at timestamp 1261476." $ do
+      consecutive "67,7,x,59,61" `shouldBe` Just 1261476
+
+    specify "1789,37,47,1889 first occurs at timestamp 1202161486." $ do
+      consecutive "1789,37,47,1889 " `shouldBe` Just 1202161486
